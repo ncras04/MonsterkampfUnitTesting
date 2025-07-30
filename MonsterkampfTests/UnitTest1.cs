ab94c48d73ee1f86b2dacb85c1efb88bdee23e8d
Nicolas Schmitt <nschmitt@duck.com>
2025-07-30 12:47:36 +0200

    init

---
+++ MonsterkampfTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MonsterkampfTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
