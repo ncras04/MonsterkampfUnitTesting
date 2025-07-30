7639643ef975afaab0d5d505ec202418a4d3c68b
traitor-joe <fordominique@hotmail.com>
2025-07-30 15:16:39 +0200

    added creation validity check

--- MonsterkampfSimulator/Program.cs
+++ MonsterkampfSimulator/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
+
+        public static Monster player1;
+        public static Monster player2;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public bool CheckValidMonster(Monster _monster1, Monster _monster2)
+        {
+            if (_monster1 == _monster2)
+            {
+                return false;
+            }
+            else return true;
+        }
+
+
     }
 }
