3b5a1cffc3a3280b6b0ca8851575fdefdb18bb3d
Nicolas Schmitt <nschmitt@duck.com>
2025-07-30 13:06:04 +0200

    CREATED: Monster base class

---
+++ MonsterkampfSimulator/Monster.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterkampfSimulator
+{
+    public class Monster
+    {
+        public float HP { get; private set; }
+        public float AP { get; private set; }
+        public float DP { get; private set; }
+
+        public Monster(float _hp, float _ap, float _dp)
+        {
+
+        }
+
+        public void TakeDamage(float _damageAmount)
+        {
+
+        }
+
+        public void Attack(Monster _monster)
+        {
+
+        }
+    }
+}
